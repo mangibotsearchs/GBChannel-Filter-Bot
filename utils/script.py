28564cd760c5c85928dbc444afa61aaf532de4be
mangibotsearchs <131441149+mangibotsearchs@users.noreply.github.com>
2023-05-08 10:54:27 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,13 +1,19 @@
 class script(object):
-    START = """**Hello {} 🤟**
+    START = """**Hᴇʏ Bᴜᴅᴅʏ! {} 😃**
    
-I am **Find Post Bot**.I am best Channel Link Search Bot! 
-I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+I'ᴍ **Find Post Bᴏᴛ**.
+
+I'ᴍ best Cʜᴀɴɴᴇʟ Link Search Bᴏᴛ! 
+I Will Filter Yᴏᴜʀ Cʜᴀɴɴᴇʟ Posts Automatically And Sᴇɴᴅ It In Yᴏᴜʀ Group Chat When Someone **Search** It.
+
+Yᴏᴜ Cᴀɴ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.☺️
+
+Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅"""
 
     HELP = """To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @GreyMatter_Bots.
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @BOTNINJA_movies.
 - After verification send /connect YourChannelID
 - Example : /connect -100xxxxxxxxxx
 - Done ✅. <b><I>Enjoy 💜❤</I></b> )
@@ -20,7 +26,7 @@ Get connected channels list with - /connections"""
     ABOUT = """Developed By @CyniteBackup
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/SAHARAN_09'>SHISHPAL</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
